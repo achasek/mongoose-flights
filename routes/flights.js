3c4325acd66a399aff31279fcd88020af60d6cda
achasek <chaseknieper@gmail.com>
2022-10-01 22:18:40 -0700

    baseline functionality complete

--- routes/flights.js
+++ routes/flights.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const flightsCtrl = require('../controllers/flights')
+
 // const flightsCtrl = require('../controllers/flights');
+router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
+router.get('/', flightsCtrl.index);
+
 
 module.exports = router;
